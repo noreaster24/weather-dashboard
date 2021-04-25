fdd0c746315c70e52ee8a41dd2fb1ed0f2c757be
Joe Porrazzo <joepa2624@me.com>
2021-04-24 21:47:04 -0700

    getWeather function and HTML elements

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,33 @@
 var weatherInputEl = document.querySelector("#weather-search");
 var searchButtonEl = document.querySelector("#search-btn");
+var weatherContainerEl = document.querySelector("#weather-container");
 
 
+var cityLocation = function () {
+    console.log("hello,world!");
+};
 
+var submitWeatherHandler = function (event) {
+    event.preventDefault();
+    // pull the value from the input form
+    var cityWeather = weatherInputEl.value.trim();
+
+    if (cityWeather) {
+        // run the city through the api fetch
+        getWeatherInfo(cityWeather);
+        weatherInputEl.value = "";
+
+    }
+    else {
+        alert("Enter a city name");
+    };
+};
+
+// fetch function to get the city weather information
 var getWeatherInfo = function (city, lon, lat) {
 
     var apiKey = "aa39a52add5cde0eecb176481fd61d11"
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=", lat, "&lon=", lon, "&appid=", + apiKey
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=", lat, "&?lon=", lon, "&appid=", + apiKey
 
     fetch(apiUrl)
         .then(function (response) {
@@ -17,13 +38,49 @@ var getWeatherInfo = function (city, lon, lat) {
                         console.log(data);
 
                         // variables for today
-                        var getDate = new Date(data.current.at ^ 1000);
+                        var getDate = new Date(data.current.dt);
                         var getTemp = data.current.temp;
                         var getWind = data.current.wind_speed;
                         var getHumidity = data.current.humidity;
                         var getUvi = data.current.uvi;
                         var getIcon = data.current.weather[0].icon;
 
+                        // create HTML elements
+                        var weather = document.createElement("div");
+                        
+                        var cityEl = document.createElement("h2");
+                        cityEl.textContent = weatherInputEl;
+
+                        var dateEl = document.createElement("h2");
+                        dateEl.textContent = "( " + getDate + " )";
+
+                        var weatherIconEl = document.createElement("img");
+                        weatherIconEl.setAttribute("src", getIcon);
+
+                        var tempEl = document.createElement("p");
+                        tempEl.textContent = "Temp: " + getTemp;
+
+                        var windEl = document.createElement("p");
+                        windEl.textContent = "Wind: " + getWind;
+
+                        var humidityEl = document.createContent("p");
+                        humidityEl.textContent = "Humidity: " + getHumidity;
+
+                        var uviEl = document.createElement("p");
+                        uviEl.textContent = "UV Index: " + getUvi;
+
+                        // append elements to the div
+                        weather.appendChild(cityEl);
+                        weather.appendChild(dateEl);
+                        weather.appendChild(weatherIconEl);
+                        weather.appendChild(tempEl);
+                        weather.appendChild(windEl);
+                        weather.appendChild(humidityEl);
+                        weather.appendChild(uviEl);
+
+                        // append the div to the page
+                        weatherContainerEl.innerHTML = "";
+                        weatherContainerEl.appendChild(weather);
                     });
             } else {
                 console.log("error: " + response.statusText);
@@ -32,3 +89,4 @@ var getWeatherInfo = function (city, lon, lat) {
 };
 
 getWeatherInfo(boston);
+searchButtonEl.addEventListener("click", submitWeatherHandler);
